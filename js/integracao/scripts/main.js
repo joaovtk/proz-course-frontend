de4327a027460c1568fdc496c32d8fd355ae446b
joaovtk <joaovtk>
2023-11-24 17:44:50 -0300

    Fazendo a ultima pratica do dia

--- js/integracao/scripts/main.js
+++ js/integracao/scripts/main.js
@@ -1,11 +1,17 @@
 (() => {
     // localizando e adicionando um evento ao botão
     let helloButton = document.getElementById("hello");
+    let input = document.getElementById("nome");
 
     helloButton.addEventListener("click", () => {
-        window.alert("Hello ser vivo");
+        let nome = input.value;
+        if(nome){
+            alert(`O seu nome é ${nome}`);
+        }else{
+            alert("Nome não informado");
+        }
     });
 
     // Pegando texto do formulario
-    
+
 })();
